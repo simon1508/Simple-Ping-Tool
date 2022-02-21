83b566cf3e6fb5b5fe420ede54714c1cfed35417
Lowly_TV <reisingersimon2@gmail.com>
2022-02-21 18:24:02 +0100

    Alle Dateien hinzufügen
    Es wurden alle Dateien des WPF Ping Tools hinzugefügt.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PingTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+
+            
+
+
+
+        }
+
+        private void addAdress(object sender, RoutedEventArgs e)
+        {
+            var eingabe = lblEingabe.Text;
+
+
+            SaveinLabel(eingabe);
+            IsSucessfull(eingabe);
+        }
+
+        private void IsSucessfull(string eingabe)
+        {
+            Ping ping = new Ping();
+
+            
+
+            var send =  ping.Send(eingabe,1000);
+
+
+            PingReply reply = send;
+
+
+            try
+            {
+               
+                    lblAusgabe_Erfolgreich.Content = send.Status;
+                    lblAusgabe_Time.Content = send.RoundtripTime;
+                    lblAusgabe_Adresse.Content = send.Address;
+               
+            }
+            catch (Exception)
+            {
+                lblAusgabe_Erfolgreich.Content = "Fehler beim Pingen";
+                
+            }
+            
+
+        }
+
+        public void SaveinLabel(string eingabe)
+        {
+            
+            lblAusgabe_Url.Content = eingabe;
+        }
+    }
+}
